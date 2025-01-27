34cef6333e913ae516ae45262e92bcf63c71617b
EgoMoose <EgoMoose@users.noreply.github.com>
2025-01-26 21:23:39 -0500

    Add raw icons + processing

---
+++ icons/process_raw.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from PIL import Image
+
+def recolorImage(img_path, r, g, b):
+    img = Image.open(img_path)
+    pixels = img.load()
+
+    for y in range(img.height):
+        for x in range(img.width):
+            (_, _, _, a) = pixels[x, y]
+            pixels[x, y] = (r, g, b, a)
+    
+    return img
+
+def recolorFolder(folder_path, new_folder_path, r, g, b):
+    if os.path.isdir(new_folder_path):
+        shutil.rmtree(new_folder_path)
+
+    os.makedirs(new_folder_path)
+
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".png"):
+            recolored = recolorImage(os.path.join(folder_path, file_name), r, g, b)
+            recolored.save(os.path.join(new_folder_path, file_name))
+
+parent_path = os.path.join(os.path.abspath(__file__), os.path.pardir)
+raw_path = os.path.join(parent_path, "raw")
+
+recolorFolder(raw_path, os.path.join(parent_path, "dark"), 225, 225, 225)
+recolorFolder(raw_path, os.path.join(parent_path, "light"), 94, 94, 94)
